878f607482b8ed6607352f38080e4165fcd6cbbb
wangf <838975843@qq.com>
2016-12-06 14:54:53 +0800

    测试

--- app/src/main/java/myapplication/myapplication/MainActivity.java
+++ app/src/main/java/myapplication/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "llllllll", Toast.LENGTH_LONG).show();
     }
 }
